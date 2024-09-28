a11287ef7ceec2fc8f84f51fe10c493622a76fca
ViniciusAlvesofc7 <166080498+ViniciusAlvesofc7@users.noreply.github.com>
2024-09-27 22:24:21 -0300

    ok

---
+++ JavaScript/aula026/promisses.js
@@ -0,0 +1,22 @@
+function testes(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+
+            const erro = false
+            if(erro){
+                reject('Erro...')
+            }else{
+                resolve("A Promisse foi resolvida com sucesso!")
+            }
+        },2000)
+    })
+}
+
+async function testes2() {
+    await testes().then(function(res){
+        alert(res)
+    })
+    alert('olá')
+}
+
+testes2()
